71c7208cc1f9d83294a4603277f69d1838c8f447
Deep Mistry <deep.mistry@nasa.gov>
2023-11-12 22:47:17 -0500

    FIRM-API: latest code push

--- python/FIRM_API.py
+++ python/FIRM_API.py
@@ -7,14 +7,27 @@ import pandas as pd
 # To do: 
 # Pass in the current date and send that date to FIRM
 
-MAP_KEY = '33c0bb32b80831ae1cb4bb94211611c8'
+def getData(date):
+    firm_api = "https://firms.modaps.eosdis.nasa.gov//api/area/csv/33c0bb32b80831ae1cb4bb94211611c8/MODIS_NRT/world/10/{}".format(date)
+    print("rest",firm_api)
+    df_firm = pd.read_csv(firm_api)
 
-firm_api = 'https://firms.modaps.eosdis.nasa.gov//api/area/csv/33c0bb32b80831ae1cb4bb94211611c8/MODIS_NRT/world/1/2023-08-09'
-df_firm = pd.read_csv(firm_api)
+    # Removing column that are not needed  
+    df_firm = df_firm.drop(['brightness', 'scan', 'track', 'acq_time', 'satellite', 
+                            'instrument', 'confidence', 'version', 'bright_t31', 'daynight'], axis=1)
 
-# Parsing out the latitude coordinates for the Maui island
-df_firm = df_firm[(df_firm['latitude'] >= 20.500) & (df_firm['latitude'] <= 21.0000)]
+    # Reordering the data
+    df_firm = df_firm[['acq_date', 'latitude', 'longitude', 'frp']]
 
-df_firm = df_firm[(df_firm['longitude'] >= -156.5000) & (df_firm['longitude'] <= -156.0000)]
+    # Parsing out the latitude coordinates for the Maui island
+    df_firm = df_firm[(df_firm['latitude'] >= 20.500) & (df_firm['latitude'] <= 21.0000)]
 
-print (df_firm)
+    # Parsing out the longitude coordinates for the Maui island
+    df_firm = df_firm[(df_firm['longitude'] >= -154.5000) & (df_firm['longitude'] <= -157.0000)]
+    print(df_firm)
+    # Parsing out the FRP 
+    df_firm = df_firm[(df_firm['frp'] > 100.00)]
+
+    # Sorting latitude
+    df_firm = df_firm.sort_values(by='latitude')
+    df_firm.to_csv('./fireData.csv', index=False, mode='a', header=False)
---
+++ python/get_fire_data.py
@@ -0,0 +1,6 @@
+import FIRM_API
+
+FIRM_API.getData('2023-11-04')
+# FIRM_API.getData('2023-08-09')
+# FIRM_API.getData('2023-08-10')
+# FIRM_API.getData('2023-08-11')
